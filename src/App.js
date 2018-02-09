9d7131a1924c2b5ba2b93590a26d2449eef12d06
Ryan Hamblin <ryanhca@gmail.com>
2018-02-09 01:12:09 -0700

    Redux integrated properly :)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>We will create an awesome game with React, Redux, and SVG!</h1>
+        <h1>{this.props.something}</h1>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  something: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    something: state.something
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider>
+    <App
+      store={createStore(
+        reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    />
+  </Provider>,
+  document.getElementById('root')
+);
